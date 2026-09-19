89b8c34d0e70510375729abcda56a2cb35cb86d7
EngMohamedNowar <mohamednowar2002@gmail.com>
2026-09-19 15:38:40 +0300

    Add BasketCustomer and BasketItem domain entities
    Introduced BasketCustomer and BasketItem classes in the ECommerce.Domain.Entities.Baskets namespace. BasketCustomer holds an Id and a collection of BasketItem objects. BasketItem includes Id, ProductName, PictureUrl, Price, and Quantity properties.

---
+++ ECommerce.Domain/Entities/Baskets/BasketCustomer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Domain.Entities.Baskets
+{
+    public class BasketCustomer
+    {
+        public Guid Id { get; set; }
+        public ICollection<BasketItem> Items { get; set; }
+
+    }
+}
---
+++ ECommerce.Domain/Entities/Baskets/BasketItem.cs
@@ -0,0 +1,17 @@
+﻿using ECommerce.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Domain.Entities.Baskets
+{
+    public class BasketItem 
+    {
+        public Guid Id { get; set; }
+        public string ProductName { get; set; } = default!;
+        public string PictureUrl { get; set; } = default!;
+
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
